7808e43414be1d63c14ff02267df72739d27b8be
Alex Soper <alexsoper29@gmail.com>
2014-12-05 08:03:39 -0500

    Updated listing to include Facebook avatar

--- src/app/listings/listings.js
+++ src/app/listings/listings.js
@@ -4,26 +4,26 @@ angular.module('mtgCentral')
 
   .controller('ListCtrl', function (){
       this.listings = [
-        { 'id' : '1', 'title' : 'Standard, Modern, and Legacy for trade', 'author' : 'Alex Soper'},
-        { 'id' : '2', 'title' : 'Commander for trade (Foils, foreign, etc)', 'author' : 'Jim James'},
-        { 'id' : '3', 'title' : 'Have: Fetches, shocks // Want: Jeskai Tempo', 'author' : 'Jason Murphy'},
-        { 'id' : '4', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '5', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '6', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '7', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '8', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '9', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '10', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '11', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '12', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '13', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '14', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '15', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '16', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '17', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '18', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '19', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
-        { 'id' : '20', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'}
+        { 'id' : '1', 'description' : 'Standard, EDH, Foreign Foil', 'author' : 'Alex Soper', 'updated' : '12/3/14'},
+        { 'id' : '2', 'description' : 'Commander, Legacy, Foreign Foil', 'author' : 'Jim James', 'updated' : '12/1/14'},
+        { 'id' : '3', 'description' : 'Standard, Modern, Legacy', 'author' : 'Jason Murphy', 'updated' : '12/2/14'}
+        // { 'id' : '4', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '5', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '6', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '7', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '8', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '9', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '10', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '11', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '12', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '13', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '14', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '15', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '16', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '17', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '18', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '19', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'},
+        // { 'id' : '20', 'title' : 'Random generic have // wants', 'author' : 'Some Guy'}
       ];
 
       // var ref = new Firebase("https://mtg-central.firebaseio.com/");
